1b20ba1ff86163c89ef1ff704795a3df1887131c
blzbrg <randomparanoid@gmail.com>
2024-01-26 00:08:22 -0500

    User init provides options and an init function
    Look for a specified filename, and load whatever namespace is in it. This allows multiple symbols
    from the namespace to be used, rather than just loading it for side effects. For example, this init
    file:
    
    ```
    (ns init
      (:require [clijmage.viewer :as viewer]
                [clijmage.main :as main]
                [clijmage.images-coll :as images-coll]
                [clijmage.util :as util]))
    
    (def startup-options
      {::main/load-image-coll-from-stdin false})
    
    (defn init []
      (viewer/apply-bindings! {(main/combination javafx.scene.input.KeyCode/P [])
                               (util/runnable (fn [] (println (images-coll/current @main/images-position))))}))
    ```
    
    Which shows that you can completely disable loading images based on stdin, if desired. You can even
    disable running the follow-up init function. Although this seems strange, it allows init files to
    skip defining it if they don't want to.

--- src/clijmage/main.clj
+++ src/clijmage/main.clj
@@ -1,7 +1,9 @@
 (ns clijmage.main
   (:require [clijmage.util :refer [runnable]]
             [clijmage.viewer :as viewer]
-            [clijmage.images-coll :as images-coll]))
+            [clijmage.images-coll :as images-coll]
+            [clojure.tools.namespace.parse]
+            [clojure.tools.namespace.file]))
 
 ;; === Images from stdin ===
 
@@ -33,6 +35,12 @@
 
 ;; === Main ===
 
+(def default-startup-options
+  {::apply-default-bindings true
+   ::load-image-coll-from-stdin true
+   ::show-initial-image true
+   ::run-user-init true})
+
 (defn user-init-path []
   (let [config-dir (if-let [config (System/getenv "XDG_CONFIG_HOME")]
                      (java.nio.file.Path/of config (into-array String []))
@@ -43,23 +51,59 @@
         (.resolve "clijmage")
         (.resolve "init.clj"))))
 
-(defn try-user-init! []
+(defn try-load-user-init! []
   (let [path (user-init-path)
         file (.toFile path)]
     (if (.canRead file)
-      (load-file (str path))
+      (let [ns-name (-> file
+                        (clojure.tools.namespace.file/read-file-ns-decl)
+                        (clojure.tools.namespace.parse/name-from-ns-decl))]
+        (load-file (str path))
+        (find-ns ns-name))
       (if (.exists file)
         ;; If the path is unreadable but exists, print a warning
         (println "Init script" path "is unreadable")))))
 
-(defn after-gui [user-init?]
-  (viewer/apply-bindings! default-bindings)
-  ;; Load image state and show the first one
-  (reset! images-position (images-coll/from-seq (lines-from-stdin)))
-  (viewer/goto! (images-coll/current @images-position))
+(defn after-gui
+  "Called after the GUI is created. This function will:
 
-  (if user-init?
-    (try-user-init!)))
+  1. Apply the default keybinds
+  2. Load a list of image paths from standard in
+  3. Show the current image in `images-position` in the viewer
+  4. Run the `init` function from the user init NS
+
+  However, the `startup-options` map in the user init NS can disable
+  each of these steps."
+  [{apply-default-bindings ::apply-default-bindings
+    load-image-coll-from-stdin ::load-image-coll-from-stdin
+    show-initial-image ::show-initial-image
+    run-user-init ::run-user-init
+    init-ns ::init-ns}]
+  (if apply-default-bindings
+    (viewer/apply-bindings! default-bindings))
+
+  ;; Load image paths
+  (if load-image-coll-from-stdin
+    (reset! images-position (images-coll/from-seq (lines-from-stdin))))
+
+  ;; Show the current image
+  (if show-initial-image
+    (if-let [pos @images-position]
+      (viewer/goto! (images-coll/current @images-position))))
+
+  ;; Run user init.
+  (if run-user-init
+    (if-let [init-fn (get (ns-map init-ns) 'init)]
+      (init-fn)
+      (println "No `init` in namespace" (ns-name init-ns)))))
 
 (defn -main [& args]
-  (javafx.application.Platform/startup (viewer/entry-point #(after-gui true))))
+  (let [init-ns (try-load-user-init!)
+        ;; If the init file was loaded, an NS was parsed from it, and startup-options is in it,
+        ;; merge into defaults.
+        startup-options (merge default-startup-options
+                               (if init-ns
+                                 (if-let [init-opt-v (get (ns-map init-ns) 'startup-options)]
+                                   @init-opt-v)))
+        startup-options-aug (assoc startup-options ::init-ns init-ns)]
+    (javafx.application.Platform/startup (viewer/entry-point #(after-gui startup-options-aug)))))
